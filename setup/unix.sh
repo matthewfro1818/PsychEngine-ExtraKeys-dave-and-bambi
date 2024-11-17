948c36cec4901c7c306fab7051e4d6a57efdb1f6
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-11-17 08:17:41 -0500

    Update and rename setup-unix.sh to unix.sh

---
+++ setup/unix.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# SETUP FOR MAC AND LINUX SYSTEMS!!!
+#
+# REMINDER THAT YOU NEED HAXE INSTALLED PRIOR TO USING THIS
+# https://haxe.org/download/version/4.2.5/
+haxelib install lime
+haxelib install openfl
+haxelib install flixel
+haxelib install flixel-addons
+haxelib install flixel-ui
+haxelib install flixel-tools
+haxelib install hxCodec
+haxelib install tjson
+haxelib git SScript https://github.com/Altronix-Team/SScript
+haxelib git flxanimate https://github.com/ShadowMario/flxanimate
+haxelib git hscript-ex https://github.com/ianharrigan/hscript-ex
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
+haxelib git faxe https://github.com/uhrobots/faxe
+haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
+haxelib git polymod https://github.com/larsiusprime/polymod.git
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
+haxelib install hxcpp-debug-server
