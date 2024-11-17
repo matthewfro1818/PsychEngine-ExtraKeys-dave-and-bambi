9e236239d7a70a65c3edd415ab6cdfcea987b587
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-11-17 06:58:16 -0500

    Update Bulid.bat

--- Bulid.bat
+++ Bulid.bat
@@ -13,10 +13,15 @@ haxelib install actuate 1.9.0
 haxelib install hxCodec 2.5.1          
 haxelib install linc_luajit
 haxelib install hscript
+haxelib install tjson
+haxelib install SScript
+haxelib install SScript
 haxelib git hscript-ex https://github.com/ianharrigan/hscript-ex
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
 haxelib git faxe https://github.com/uhrobots/faxe
+haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
 haxelib git polymod https://github.com/larsiusprime/polymod.git
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib install hxcpp-debug-server
 haxelib list
